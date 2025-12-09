6cb32505d5e650674d5d1e011ee02b1874b20e41
William Best <148398824+WillBest06@users.noreply.github.com>
2025-12-09 15:53:53 +0000

    add findNode method

--- tree.py
+++ tree.py
@@ -19,7 +19,22 @@ class Tree:
 
         return currentNode
 
+    def findNode(self, currentNode, searchValue):
+        if currentNode == None:
+            return searchValue, "is not present in this tree"
+
+        if currentNode.data == searchValue:
+            return searchValue, "is present in this tree"
+
+        if searchValue > currentNode.data:
+            return self.findNode(currentNode.right, searchValue)
+        elif searchValue <= currentNode.data:
+            return self.findNode(currentNode.left, searchValue)  
+
 myTree = Tree()
 myTree.root = myTree.addNode(myTree.root, 1)
 myTree.addNode(myTree.root, 2)
 myTree.addNode(myTree.root, 3)
+
+print(myTree.findNode(myTree.root, 3))
+print(myTree.findNode(myTree.root, 4))
