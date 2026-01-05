0ddc22154473edfbd8ee4a9bbbec70155f7da54c
William Best <148398824+WillBest06@users.noreply.github.com>
2026-01-05 17:41:33 +0000

    add new test to node test suite

--- tests/node_test.py
+++ tests/node_test.py
@@ -7,16 +7,20 @@ class NodeTests(unittest.TestCase):
         self.assertEqual(testNode.data, None)
         self.assertEqual(testNode.left, None)
         self.assertEqual(testNode.right, None)
+        
+    def test_node_init_with_params(self):
+        leftChild = Node(1)
+        rightChild = Node(3)
+        parent = Node(2, leftChild, rightChild)
+
+        self.assertEqual(parent.data, 2)
+        self.assertIs(parent.left, leftChild)
+        self.assertIs(parent.right, rightChild)
 
     def test_node_data_change(self):
         testNode = Node()
         testNode.data = 1
         self.assertEqual(testNode.data, 1)
-
-    # def test_node_pointer_access_for_empty_pointer(self):
-    #     testNode = Node()
-    #     leftVal = testNode.left
-    #     self.assertRaises(AttributeError, leftVal.data)
 
 if __name__ == '__main__':
     unittest.main()
